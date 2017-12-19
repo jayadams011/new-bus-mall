cf889692f406d95177bef28d8e572fd8de8dec41
jayadams011 <j.adams011@hotmail.com>
2017-12-19 14:06:35 -0800

    added more js but still not working

--- app.js
+++ app.js
@@ -1,53 +1,76 @@
 'use strict';
 
 //var getPics = document.getElementsByClassName('pics');
-
+var pictures = [];
+var allcounter = 0;
 var allStuff = ['img/bag.jpg','img/banana.jpg','img/bathroom.jpg','img/boots.jpg','img/breakfast.jpg','img/bubblegum.jpg','img/chair.jpg','img/cthulhu.jpg','img/dog-duck.jpg','img/dragon.jpg','img/dragon.jpg','img/pen.jpg','img/pet-sweep.jpg','img/scissors.jpg','img/shark.jpg','img/sweep.png','img/tauntaun.jpg','img/unicorn.jpg','img/usb.gif','img/water-can.jpg','img/wine-glass.jpg'];
 
 //stuff.allStuff = [];
 // need to figure out how to get name and filepath from allStuff above.
-// how do I tie allStuff to the functions below. 
+// how do I tie allStuff to the functions below.
 
 function Stuff(name,filepath){
   this.name = name;
   this.filepath = filepath;
-  this.name = filepath.split('/')[0];
+  this.name = filepath.split('/')[1].split('.')[0];
   this.numClicks = 0;
   this.numShown = false;
   Stuff.allStuff.push(this);
 }
-new Stuff ();
 
+var newStuff (){
+  for(var i = 0, i < allStuff.length, i++){
+  new Stuff(allStuff[i]);
+  }
+}
 
-var pick6img = function () {
+var pick6img = function() {
   var counter = 0;
   var images = [];
   while (counter < 6){
-    var imageRun = [Math.floor(Math.random() * allStuff.length)];
+    var index = [Math.floor(Math.random() * allStuff.length)];
+    var imageRun = pictures[index];
     //console.log(imagerun)
     if (imageRun.shown === false){
       imageRun.numShown++;
       counter++;
       imageRun.shown = true;
       images.push(imageRun);
+      console.log(images);
+    }
+  }
+
+  for (var i = 0; i < pictures.length, i++);
+    if (i !== index){
+      pictures[i].shown = false;
     }
   }
   return images;
-};
+}
+
+
+function begining(){
+  Stuff();
+  placeOne = document.getElementById('placeOne').innerHTML = "img src=\"img/" + pictures[0];
+  placeTwo = document.getElementById('placeTwo').innerHTML = "img src=\"img/" + pictures[1];
+  placeThree = document.getElementById('placeThree').innerHTML = "img src=\"img/" + pictures[2];
+}
+
 
 function render (){
   var images = pick6img();
-  for (var i = 0; i < images.length; i++){
-    document.getElementById('placeOne');
-    pics[i].innerHTML = '<img src = images[i]>';
+  document.getElementById('placeOne').innerHTML = "<"img src=\"img/" + images[0]>;"
+  document.getElementById('placeTwo').innerHTML = "<"img src=\"img/" + images[1]>;"
+  document.getElementById('placeThree').innerHTML = "<"img src=\"img/" + images[2]>;"
+}
+placeOne.addEventListner('click',render);
+placeTwo.addEventListener('click',render);
+placeThree.addEventListener('click',render);
 
-    document.getElementById('placeTwo');
-    pics[i].innerHTML = '<img src = images[i]>';
-
-    document.getElementById('placeThree');
-    pic[i].innerHTML = '<img src = images[i]>';
-
-  }
-};
-render();
-div.addEventListener('click', render);
+function times({
+  while (gcounter<25){
+    render()
+    }
+}
+beginning();
+times();
