fac3b4f266884013bb8b5953222f40313c1e6a07
jayadams011 <j.adams011@hotmail.com>
2017-12-19 22:43:02 -0800

    added code for charts. Still needs some work.

--- app.js
+++ app.js
@@ -76,7 +76,7 @@ var counter = 0;
 placeOne.addEventListener('click', function(){
   allPictureObjects[randOne].numClicks += 1;
   counter += 1;
-  if (counter >= 10) {
+  if (counter >= 25) {
     // Remove the images
     document.getElementById('imageHolder').remove();
     // Collect Data
@@ -91,12 +91,30 @@ placeOne.addEventListener('click', function(){
 placeTwo.addEventListener('click', function(){
   allPictureObjects[randTwo].numClicks += 1;
   counter += 1;
+  if (counter >= 25) {
+    // Remove the images
+    document.getElementById('imageHolder').remove();
+    // Collect Data
+    collectData();
+    console.log('numberOfTimesShown :: ', numberOfTimesShown);
+    console.log('numberOfTimesClicked :: ', numberOfTimesClicked);
+    // Show chart
+  }
   showImages();
 });
 
 placeThree.addEventListener('click', function(){
   allPictureObjects[randThree].numClicks += 1;
   counter += 1;
+  if (counter >= 25) {
+    // Remove the images
+    document.getElementById('imageHolder').remove();
+    // Collect Data
+    collectData();
+    console.log('numberOfTimesShown :: ', numberOfTimesShown);
+    console.log('numberOfTimesClicked :: ', numberOfTimesClicked);
+    // Show chart
+  }
   showImages();
 });
 
@@ -116,3 +134,43 @@ var collectData = function() {
 //     data: data,
 //     options: options
 // });
+
+var canvas = document.getElementById('chart');
+var ctx = canvas.getContext('2d');
+var chart = new Chart(ctx, {
+
+  type: 'bar',
+  data: {
+    labels: ['allPictureObjects[i]'],
+    datasets: [{
+      label: ['Times Clicked'],
+      data: ['numberOfTimesShown[i]'],
+      backgroundColor: [
+        'rgba(255, 99, 132, 0.2)',
+        'rgba(54, 162, 235, 0.2)',
+        'rgba(255, 206, 86, 0.2)',
+        'rgba(75, 192, 192, 0.2)',
+        'rgba(153, 102, 255, 0.2)',
+        'rgba(255, 159, 64, 0.2)'
+      ],
+      borderColor: [
+        'rgba(255,99,132,1)',
+        'rgba(54, 162, 235, 1)',
+        'rgba(255, 206, 86, 1)',
+        'rgba(75, 192, 192, 1)',
+        'rgba(153, 102, 255, 1)',
+        'rgba(255, 159, 64, 1)'
+      ],
+      borderWidth: 1
+    }]
+  },
+  options: {
+    scales: {
+      yAxes: [{
+        ticks: {
+          beginAtZero:true
+        }
+      }]
+    }
+  }
+});
